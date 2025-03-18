de819ea5844835100a938989a98eeb47f44c80d6
Seli888 <s.d.selivanov@yandex.ru>
2025-03-18 19:14:04 +0700

    задача3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,15 +13,18 @@ public class App {
         someNumbers.add(-2);
         someNumbers.add(-3);
         someNumbers.add(5);
+        someNumbers.add(0);
         ArrayList <Integer> firstList = new ArrayList<>();
         ArrayList <Integer> secondList = new ArrayList<>();
 
 
             for (int i = 0; i < someNumbers.size(); i++){
-                if (someNumbers.get(i) <= 0) {
+                if (someNumbers.get(i) < 0) {
                     firstList.add(someNumbers.get(i));
                 } else {
                     secondList.add(someNumbers.get(i));
+                } if (someNumbers.get(i) == 0) {
+                    System.out.println("Элемент списка номер: " + i + " равен 0" );
                 }
             }
         System.out.println(firstList);
