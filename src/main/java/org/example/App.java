6b521cff5543cbcde094c79ff4f6c8fc4a49f0a2
Seli888 <s.d.selivanov@yandex.ru>
2025-03-18 18:31:55 +0700

    задача2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,14 +1,21 @@
 package org.example;
-
+import lombok.Data;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
+@Data
 public class App {
 
     public static void main( String[] args ) {
+        ArrayList<Person> persona = new ArrayList<>();
+        persona.add(new Person("Kolya", 15));
+        persona.add(new Person("Vanya", 17));
+        persona.add(new Person("Dima", 12));
+        persona.add(new Person("Andrey", 10));
 
-        ArrayL arrayL = new ArrayL();
-        arrayL.ArrayL();
-
+        Collections.sort(persona);
+        System.out.println(persona);
     }
 }
